7af0fc3d16f59e577e544da834a497898e1e305d
Dustin Schuette <dustintschuette@gmail.com>
2021-10-27 13:12:28 -0700

    finished lab

--- lab09/dns.py
+++ lab09/dns.py
@@ -14,7 +14,7 @@
 #    to produce that optional, more modern header)
 
 
-from dns_tools import dns  # Custom module for boilerplate code
+from dns_tools import dns, dns_header_bitfields  # Custom module for boilerplate code
 
 import argparse
 import ctypes
@@ -22,6 +22,7 @@ import random
 import socket
 import struct
 import sys
+
 
 def main():
 
@@ -49,12 +50,73 @@ def main():
     # ---------
     # STUDENT TO-DO
     # ---------
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Generate DNS request message
     # ---------
     # STUDENT TO-DO
     # ---------
+    raw_bytes = bytearray()
+    if qtype == 'A':
+        qtype = 1
+    else: 
+        qtype = 28
+        # 0 28
+    
+    #pack and unpack
+    #struct.pack("BH", val1, val2)
+    #B = 1, H = 2, L = 4
+    #(val1, val2) = struct.unpack("BH", raw_bytes)
+    # use ! for big endian format
+
+
+    flags = dns_header_bitfields()
+    flags.qr = 0
+    flags.opcode = 0
+    flags.aa = 0
+    flags.tc = 0
+    flags.rd = 1
+    flags.ra = 0
+    flags.reserve = 2
+    flags.rcode = 0
+
+    Q = 1
+    Ans = 0
+    Auth = 0
+    Add = 0
+
+
+    name = qname.split('.')
+    raw_name = bytearray()
+    for i in name:
+        #print(len(name[i]))
+        raw_name += struct.pack("!B", len(i))
+        piece = i.encode()
+        raw_name += piece
+    raw_name += struct.pack("!B", 0)
+    messageId = struct.pack("!H", random.randrange(0, 65535, 1))
+    #messageID
+    raw_bytes += (messageId)
+    #Flags
+    raw_bytes += bytes(flags)
+    #QDCount
+    raw_bytes += (struct.pack("!H", Q))
+    #ANCount
+    raw_bytes += (struct.pack("!H", Ans))
+    #NSCount
+    raw_bytes += (struct.pack("!H", Auth))
+    #ARCount
+    raw_bytes += (struct.pack("!H", Add))
+    #Qname
+    raw_bytes += (raw_name)
+    #QType
+    raw_bytes += (struct.pack("!H", qtype))
+    #QClass
+    raw_bytes += (struct.pack("!H", 1))
+
+
+
+
 
 
     # Send request message to server
@@ -62,23 +124,27 @@ def main():
     # ---------
     # STUDENT TO-DO
     # ---------
-
+    bytes_sent = s.sendto(raw_bytes, server_address)
+    max_bytes = 4096
 
     # Receive message from server
     # (Tip: use recvfrom() function for UDP)
     # ---------
     # STUDENT TO-DO
     # ---------
+    (raw_bytes, src_addr) = s.recvfrom(max_bytes)
+
 
 
     # Close socket
     # ---------
     # STUDENT TO-DO
     # ---------
+    s.close()
 
 
     # Decode DNS message and display to screen
-    dns.decode_dns(raw_bytes)
+    print(repr(dns.decode_dns(raw_bytes)))
 
 
 if __name__ == "__main__":
